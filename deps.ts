b5858c06774bedeb92e752f8e2ca0d1e6a8e2464
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2021-05-07 14:51:36 +0000

    add base of REST

---
+++ deps.ts
@@ -0,0 +1,7 @@
+// Sift is a routing and utility library for Deno Deploy.
+export {
+  json,
+  serve,
+} from "https://deno.land/x/sift@0.3.0/mod.ts";
+export * from "https://raw.githubusercontent.com/discordeno/discordeno/main/mod.ts";
+export { decode } from "https://deno.land/std/encoding/base64url.ts";
---
+++ mod.ts
@@ -0,0 +1,37 @@
+import { decode, json, rest, serve, setApplicationId } from "./deps.ts";
+
+const token = Deno.env.get("DISCORD_TOKEN");
+rest.token = `Bot ${token}`;
+setApplicationId(
+  new TextDecoder().decode(decode(token?.split(".")[0] || "")) || "",
+);
+
+serve({
+  "/": main,
+});
+
+async function main(request: Request) {
+  const authorization = request.headers.get("Authorization")!;
+
+  const secret = Deno.env.get("GAMER_REST_SECRET");
+  if (!secret || !authorization || authorization !== secret) {
+    return json({
+      error: "Invalid authorization provided.",
+    }, { status: 401 });
+  }
+
+  const payload = JSON.parse(await request.text());
+
+  await rest.processRequest({
+    url: payload.url,
+    method: request.method,
+    respond: (payload) => {
+      json(payload.body ? JSON.parse(payload.body) : {}, {
+        status: payload.status,
+      });
+    },
+    reject: (error) => json(error as Record<string, unknown>),
+  }, payload).catch(console.error);
+
+  return json({ error: "Bad request" }, { status: 400 });
+}
